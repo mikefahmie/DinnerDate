3ec973f7fd289fa3c06c99a06b4f2f7e3bcb8842
Mike Fahmie <fahmiemike@gmail.com>
2025-08-08 10:21:35 -0400

    Edited icon for add/edit filters

--- components/filters/FilterChips.tsx
+++ components/filters/FilterChips.tsx
@@ -171,7 +171,7 @@ const FilterChips: React.FC<FilterChipsProps> = ({
             onPress={onEditFilters}
           >
             <Icon
-              name="gear"
+              name="filter"
               type="font-awesome-5"
               size={12}
               color={theme.colors.textPrimary}
